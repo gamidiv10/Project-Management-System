eddf30bbb59729e21450f7f1badd4729687c6ee7
Satya Kumar Itekela <st798799@dal.ca>
2020-06-22 20:04:56 -0300

    Changes to Task Management Styling

--- src/App.js
+++ src/App.js
@@ -6,11 +6,25 @@ import "./App.scss";
 import Navigationbar from "./Components/Navbar/Navbar";
 import userContext from "./Context/userContext";
 import tasksItemsContext from "./Context/tasksItemsContext";
+import Task from "./Components/Task/Task";
+
+const task = {
+  projectName: "project1",
+  issueType: "Bug",
+  taskSummary: "Bug1",
+  assigneeName: "Max",
+  taskPriority: "highest",
+};
 
 function App() {
   const [isNavbar, setIsNavbar] = useState(true);
   const [user, setUser] = useState("user");
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      content: <Task {...task} />,
+    },
+  ]);
 
   useEffect(() => {
     if (window.location.pathname === "/error") {
--- src/Components/Comments/Comments.js
+++ src/Components/Comments/Comments.js
@@ -5,17 +5,14 @@ import { Button } from "react-bootstrap";
 export const Comments = () => {
   return (
     <div className="comments-container">
-      <div className="issueHeading">
-        Comments
-      </div>
+      <div className="issueHeading">Comments</div>
       <div className="commentsTextArea">
-      <textarea placeholder="Add a comment" rows="4"></textarea>
+        <textarea placeholder="Add a comment" rows="4"></textarea>
       </div>
 
       <div className="buttons">
-      <Button>Add Comment</Button>
+        <Button>Add Comment</Button>
       </div>
-
     </div>
   );
 };
