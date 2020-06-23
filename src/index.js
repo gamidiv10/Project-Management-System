614a015d2608074cb34eee911da89f711bee16cc
Sneh Jogani <sjogani16@gmail.com>
2020-06-22 23:14:40 -0300

    scss file changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./scss/index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import "./scss/index.scss";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-big-calendar/lib/css/react-big-calendar.css"
 
