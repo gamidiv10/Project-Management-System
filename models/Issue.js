55979cf95666e53253e634bb9ff7a7e9d9c56f06
Vamsi Gamidi <vm709690@dal.ca>
2020-07-25 22:39:19 -0300

    FIxed Heroku Build Failure

--- models/Issue.js
+++ models/Issue.js
@@ -1,27 +1,26 @@
-import { Schema, model} from 'mongoose'
+const mongoose = require("mongoose");
 
-const IssueSchema = new Schema({
-    type: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-    },
-    isOpen: {
-        type: Boolean,
-        required: true,
-        default: true
-    },
-    isPartOfSprint: {
-        type: Boolean,
-        required: true,
-        default: false
-    }
-})
-
-export default model('Issue', IssueSchema)
+const IssueSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  isOpen: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  isPartOfSprint: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+module.exports = mongoose.model("Issue", IssueSchema);
