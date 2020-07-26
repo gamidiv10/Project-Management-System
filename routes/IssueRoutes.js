f38decc5fbcf5bfbd99e6540809a158f68d78a9c
Vamsi Gamidi <vm709690@dal.ca>
2020-07-25 22:17:19 -0300

    Changed node files to ES5

--- routes/IssueRoutes.js
+++ routes/IssueRoutes.js
@@ -1,16 +1,16 @@
-import { Router } from "express"
-import {
-    createIssue,
-    updateIssue,
-    deleteIssue,
-    getIssues
-} from "../controllers/IssueController"
+const Router = require("express");
+const {
+  createIssue,
+  updateIssue,
+  deleteIssue,
+  getIssues,
+} = require("../controllers/IssueController");
 
-const router = Router()
+const router = Router();
 
-router.post('/createIssue', createIssue)
-router.put('/updateIssue', updateIssue)
-router.delete('/deleteIssue', deleteIssue)
-router.post('/getIssues', getIssues)
+router.post("/createIssue", createIssue);
+router.put("/updateIssue", updateIssue);
+router.delete("/deleteIssue", deleteIssue);
+router.post("/getIssues", getIssues);
 
-export default router
+module.exports = router;
